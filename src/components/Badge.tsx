4569573b223c90f4bbe57d6b9652899c3d131d69
Paul B <pebartocillo@apper.ph>
2023-09-11 10:11:15 +0800

    chore: push build v1

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -54,14 +54,14 @@ export const Badge = (props: Props) => {
     }}>Powered by
       <a
         ref={liteBadge}
-        href={'https://flowiseai.com'}
+        href={'https://www.hellomsg.ai'}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
         id="lite-badge"
         style={{ "font-weight": 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Flowise</span>
+        <span> HelloMsg AI</span>
       </a>
     </span>
   )
